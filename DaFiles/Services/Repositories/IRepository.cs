72585d43698372381e1b4b7bc41c25a67f10f1f9
Dawid Wąsowski <dawider110@gmail.com>
2025-11-27 01:18:28 +0100

    formatting

--- DaFiles/Services/Repositories/IRepository.cs
+++ DaFiles/Services/Repositories/IRepository.cs
@@ -1,16 +1,17 @@
-﻿using System;
+﻿using DaFiles.Models;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using DaFiles.Models;
 
 namespace DaFiles.Services.Repositories;
 
 public interface IRepository : IDisposable
 {
-    public string GetRootPath();
-    public string CombinePath(string path1, string path2);
-    public Task<Directory> ReadDirectoryAsync(string path);
-    public Task<List<DirectoryItem>> ReadDirectoryContentsAsync(string path);
+    string GetRootPath();
+
+    string CombinePath(string path1, string path2);
+
+    Task<Directory> ReadDirectoryAsync(string path);
+
+    Task<List<DirectoryItem>> ReadDirectoryContentsAsync(string path);
 }
