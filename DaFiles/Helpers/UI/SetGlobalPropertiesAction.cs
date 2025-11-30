ee284db9d8c899317b6a758b43ec2d8e1d03c859
Dawid Wąsowski <dawider110@gmail.com>
2025-11-30 22:35:32 +0100

    Fix fringing on monitors with BGR subpixel layout

---
+++ DaFiles/Helpers/UI/SetGlobalPropertiesAction.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia.Controls;
+using Avalonia.Media;
+using Avalonia.Xaml.Interactivity;
+
+namespace DaFiles.Helpers.UI;
+
+public class SetGlobalPropertiesAction : StyledElementAction
+{
+    public TextRenderingMode TextRenderingMode { get; init; }
+
+    public override object? Execute(object? sender, object? parameter)
+    {
+        if (sender is TopLevel topLevel)
+        {
+            RenderOptions.SetTextRenderingMode(topLevel, TextRenderingMode);
+        }
+        return null;
+    }
+}
