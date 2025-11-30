9d54b718a216ff5c51679e320dca3e6060c78213
Dawid Wąsowski <dawider110@gmail.com>
2025-11-30 23:01:38 +0100

    Display item modified date in local timezone

---
+++ DaFiles/Converters/DateTimeToLocalTimeZoneConverter.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia.Data;
+using Avalonia.Data.Converters;
+using System;
+using System.Globalization;
+
+namespace DaFiles.Converters;
+
+public class DateTimeToLocalTimeZoneConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is DateTimeOffset dateTimeOffset)
+            return dateTimeOffset.LocalDateTime;
+
+        return new BindingNotification(new InvalidCastException(), BindingErrorType.Error);
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
